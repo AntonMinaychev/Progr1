e06fd4ccb69e5dcd47acdcc99d09a00b71f567bd
AntonMinaychev <mail.box42@yandex.ru>
2022-07-08 03:16:15 +0600

    Ex1 done

---
+++ Ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Insert first number");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Insert second number");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+
+{
+    Console.WriteLine("Biggest is ");
+    Console.Write(a);
+}
+
+else
+
+{
+    Console.WriteLine("Biggest is ");
+    Console.Write(b);
+}
